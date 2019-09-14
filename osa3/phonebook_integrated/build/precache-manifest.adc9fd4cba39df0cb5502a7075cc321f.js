5ae85c0bc394a9c7f36f1c8202a849aee735c7b4
asd <asd@localhost.com>
2019-09-14 13:32:24 +0300

    push part3 files

---
+++ osa3/phonebook_integrated/build/precache-manifest.adc9fd4cba39df0cb5502a7075cc321f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19bc5d212a148a9c18fb32e438c1e2e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11155862b14d8dd24cbb",
+    "url": "/static/css/main.7b5b0ec6.chunk.css"
+  },
+  {
+    "revision": "7e912b665cf680fab64d",
+    "url": "/static/js/2.412f75cb.chunk.js"
+  },
+  {
+    "revision": "11155862b14d8dd24cbb",
+    "url": "/static/js/main.0461ae39.chunk.js"
+  },
+  {
+    "revision": "33857bf9d4c923733bd3",
+    "url": "/static/js/runtime~main.39cf688f.js"
+  }
+]);
